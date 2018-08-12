8ba14baeb7f3c0af5fa430c2c3715bcc6ba292b2
anubhav-local <testemail>
2018-08-12 13:32:08 +0800

    Updated credentials

--- server.js
+++ server.js
@@ -2,20 +2,7 @@ const express = require("express");
 const app = express();
 
 const firebase = require("firebase-admin");
-const serviceAccount = {
-  "type": "service_account",
-  "project_id": "visual-vector",
-  "private_key_id": process.env.priv_key_id,
-  "private_key": process.env.priv_key,
-  "client_email": process.env.client_email,
-  "client_id": process.env.client_id,
-  "auth_uri": process.env.auth_uri,
-  "token_uri": process.env.token_uri,
-  "auth_provider_x509_cert_url": process.env.auth_cert_url,
-  "client_x509_cert_url": process.env.client_cert_url
-}
-
-// require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.accJSON);
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
